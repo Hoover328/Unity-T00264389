790d7302237e0eb136de9a42c80f578c2d0206a1
STUDENT Harvey D. Smith <Harvey.D.Smith@students.ittralee.ie>
2024-12-11 12:34:14 +0000

    Signed-off-by: STUDENT Harvey D. Smith <Harvey.D.Smith@students.ittralee.ie> Added collectibles, fixed errors

---
+++ my first unity project/Assets/Item.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Inventory Inventory = other.GetComponent<Inventory>();
+
+        if (Inventory != null)
+        {
+            Inventory.Collect();
+            gameObject.SetActive(false);
+        }
+
+
+    }
+
+}
+
